31b0b402626a9d33057dc491a4b75beb08353d5e
dfanz0r <dfanz0r@gmail.com>
2026-03-21 11:04:02 -0500

    Remove debug output for projection matrix from Main method

--- MeshTool.UI/Program.cs
+++ MeshTool.UI/Program.cs
@@ -12,7 +12,6 @@ class Program
     public static void Main(string[] args)
     {
         var proj = Silk.NET.Maths.Matrix4X4.CreatePerspectiveFieldOfView(MathF.PI / 3.0f, 1.0f, 0.1f, 100.0f);
-        System.IO.File.WriteAllText("proj_output.txt", $"M33: {proj.M33}, M43: {proj.M43}, M34: {proj.M34}, M44: {proj.M44}");
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
